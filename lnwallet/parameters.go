2ec0ed26df2ade8489654a72a021ac8d75376030
Matheus Degiovani <opensource@matheusd.com>
2019-04-10 17:43:44 -0300

    lnwallet: Fix rounding in dust calculation
    This fixes the integer math of dust calculation so that relay fees lower
    than 1000 don't cause a rounding error.

--- lnwallet/parameters.go
+++ lnwallet/parameters.go
@@ -34,8 +34,10 @@ func DustThresholdForRelayFee(relayFeeRate AtomPerKByte) dcrutil.Amount {
 	scriptSize := P2PKHPkScriptSize
 	totalSize := outputSize + 1 + scriptSize + inputRedeemSize
 
+	// Calculate the relay fee for this test tx in atoms, given its
+	// estimated totalSize and the provided relayFeeRate in atoms/KB.
+	relayFee := totalSize * int64(relayFeeRate) / 1000
+
 	// Threshold for dustiness is determined as 3 times the relay fee.
-	byteFee := dcrutil.Amount(relayFeeRate) / 1000
-	minRelayFee := dcrutil.Amount(totalSize) * byteFee
-	return 3 * minRelayFee
+	return dcrutil.Amount(3 * relayFee)
 }
